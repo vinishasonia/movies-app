ec9175c65ff11806f3dbb2338ecc92b316f7816b
Vinisha <soniavinisha@gmail.com>
2024-09-16 11:18:31 +0530

    done

--- src/Component/Pagination.jsx
+++ src/Component/Pagination.jsx
@@ -1,5 +1,19 @@
-function Pagination() {
-  return <div className="bg-gray-400">Pagination</div>;
+import { FaArrowRight } from "react-icons/fa6";
+import { FaArrowLeftLong } from "react-icons/fa6";
+function Pagination({ handlePrev, handleNext, pageNo }) {
+  return (
+    <>
+      <div className="bg-gray-400 p-4 mt-8 flex justify-center">
+        <div onClick={handlePrev} className=" px-8 hover:cursor-pointer">
+          <FaArrowLeftLong />
+        </div>
+        <div className="font-bold">{pageNo}</div>
+        <div onClick={handleNext} className="px-8 hover:cursor-pointer">
+          <FaArrowRight />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Pagination;
